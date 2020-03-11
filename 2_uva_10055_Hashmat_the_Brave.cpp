220095a3c19d1d023ccdc008b89fe64dcf1a4722
施崇祐(Eric) <f14051172@gs.ncku.edu.tw>
2020-03-12 00:07:51 +0800

    (1 star)Finish Q10055

---
+++ 2_uva_10055_Hashmat_the_Brave.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    long long first=0, second=0;
+    while (cin >> first >> second){
+        cout << abs(first-second) << endl;
+    }
+    
+    return 0;
+}
